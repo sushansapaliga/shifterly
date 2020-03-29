9e1a001965a63059926b0dde40981ad23aec9228
Sushan Sapaliga <sushansapaliga@gmail.com>
2020-03-30 01:19:20 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
-  firebase:{
-    apiKey: "AIzaSyDhVmNYg-qoi4pjt2Eh4sjK-lUPs1J5LYM",
-    authDomain: "shifterly-7cb53.firebaseapp.com",
-    databaseURL: "https://shifterly-7cb53.firebaseio.com",
-    projectId: "shifterly-7cb53",
-    storageBucket: "shifterly-7cb53.appspot.com",
-    messagingSenderId: "760805513840",
-    appId: "1:760805513840:web:b59c50a98872a3788772b5"
-  }
+<=========================================>
+  firebase api key here
+  <============================>
 };
